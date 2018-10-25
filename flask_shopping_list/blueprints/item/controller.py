0bc318c6f65b6eb45758142ab94fb725782e31f4
Vlad Saveluc <vlad.saveluc@gmail.com>
2018-10-25 15:02:36 -0700

    render from template

--- flask_shopping_list/blueprints/item/controller.py
+++ flask_shopping_list/blueprints/item/controller.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from app import Item
 items_bp = Blueprint('items', __name__, url_prefix='/items')
 
 @items_bp.route('/')
 @items_bp.route('/list')
 def get_all():
-	items = list(map(lambda x: x.serialize(), Item.query.all()))
-	return jsonify(items=items)
+	return render_template('items.html', items=Item.query.all())
