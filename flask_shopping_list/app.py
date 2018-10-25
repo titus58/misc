c196d4ba3e6c39c0a230e23498fca0d641c691a7
Vlad Saveluc <vlad.saveluc@gmail.com>
2018-10-25 11:48:51 -0700

    return items from db in json format

--- flask_shopping_list/app.py
+++ flask_shopping_list/app.py
@@ -1,7 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80))
+	quantity = db.Column(db.String(100))
+
+	def __repr__(self):
+		return f"<{self.name}, {self.quantity}>"
+
+	def serialize(self):
+		return {'name': self.name, 'quantity': self.quantity}
+	
 
 @app.route('/')
 def hello_world():
-	return 'Hello'
+	items = list(map(lambda x: x.serialize(), Item.query.all()))
+	return jsonify(items=items)
